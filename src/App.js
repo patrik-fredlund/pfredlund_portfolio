358e5449eb136c3b894f8d4fec7b22d2ede177d8
patrik-fredlund <patrikfredlunds@gmail.com>
2021-02-04 21:01:10 +0100

    added pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
 import './App.css';
+import About from './About';
+import Contact from './Contact';
+import Projects from './Projects';
+import Home from './Home';
 
 function App() {
   return (
     <div>
-      <h1>Patrik Fredlund</h1>
+      <h2>Patrik Fredlund</h2>
+      <Home />
+      <Projects />
+      <About />
+      <Contact />
     </div>
-  );
+  )
+ 
+  
 }
 
 export default App;
