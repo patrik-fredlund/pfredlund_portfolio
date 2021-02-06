e498f51d8e3d9ee696f070e736f27fbac6d19e3f
patrik-fredlund <patrikfredlunds@gmail.com>
2021-02-06 22:14:00 +0100

    added background img

--- src/App.js
+++ src/App.js
@@ -5,24 +5,29 @@ import Home from './Pages/Home';
 import Navbar from './Navbar/Navbar';
 import About from './Pages/About';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import ProfileImage from "./putte.jpg";
 
 function App() {
   return (
+    
     <Router>
       <Navbar />
       <Switch>
         <Route path='/about'>
           <About />
+          {<img src={ProfileImage} className='profileImage' alt="Patrik profile picture" />}
         </Route>
         <Route path='/projects'>
           <Projects />
+          {<img src={ProfileImage} className='profileImage' alt="Patrik profile picture" />}
         </Route>
         <Route path='/contact'>
           <Contact />
+          {<img src={ProfileImage} className='profileImage' alt="Patrik profile picture" />}
         </Route>
         <Route path='/'>
           <Home />
+          {<img src={ProfileImage} className='profileImage' alt="Patrik profile picture" />}
         </Route>
       </Switch>
     </Router>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,5 @@
+
+
 function Home() {
   return <h1>Home</h1>;
 }
