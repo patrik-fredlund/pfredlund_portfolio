6fb7895588d94f9e97566b873cbcd4b1bffb561f
patrik-fredlund <patrikfredlunds@gmail.com>
2021-02-07 13:49:26 +0100

    modifed navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,16 +2,6 @@ import { Nav, NavLink, Bars, NavMenu } from './NavbarElements';
 
 import styled from 'styled-components';
 
-const StyledUL = styled.ul`
-  list-style: none;
-  display: flex;
-  justify-content: space-between;
-  font-family: roboto;
-
-  & > li {
-    padding: 0 10px 0 0px;
-  }
-`;
 
 function Navbar() {
   return (
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <div className='homeContainer'> 
 <h1 className='hello'>Hi There!</h1>
 <hr className='line'></hr>
-<h2 className='welcome'>Welcome to my Portfolio coolio!</h2>
+<h2 className='welcome'>Welcome to my Portfolio Coolio!</h2>
 <p className='info'>My name is Patrik, but you can call me Putte.<br></br>I am an songwriting graphic designer turned frontend developer.<br></br> I love my wife, my guitar, typography, beautiful words and dogs.<br></br> In that exact order.</p>
 </div>
   )
